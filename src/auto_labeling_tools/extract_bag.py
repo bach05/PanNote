9e9fa2ae526bd532b76a91cab32b68e1c0b375c2
Alberto Bacchin <alberto.bacchin.1@phd.unipd.it>
2023-09-08 18:17:43 +0200

    fix synchronization

--- src/auto_labeling_tools/extract_bag.py
+++ src/auto_labeling_tools/extract_bag.py
@@ -6,15 +6,16 @@ from util.bag_extractor import Bag_manager
 
 if __name__ == "__main__":
 
-    path_bag = "/media/iaslab/Elements/bag/lab_indoor_1.bag"
+    path_bag = "/media/iaslab/Elements/bag/hospital3_static.bag"
+    #path_bag = "/home/iaslab/ROS_AUTOLABELLING/AutoLabeling/src/auto_calibration_tools/bags/hospital.bag"
     t_laser = "/scan"
     t_img = "/theta_camera/image_raw"
-    path_out = "../manual_labelling/lab_indoor_1"
+    path_out = "../manual_labelling/hospital3_static"
 
     path_laser = osp.join(path_out, "laser.csv")
     path_images = osp.join(path_out, "img")
 
     # extract data from bag
     bm = Bag_manager(path_bag, t_img, t_laser)
-    bm.extract_bag(path_out)
+    bm.extract_bag(path_out, step_size=7)
 
